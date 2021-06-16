a8224090b40cef32120f33788b7e516eb139729a
muhsin <muhsinur.chowdhury@selise.ch>
2021-06-16 02:36:46 -0600

    update packages

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const cors = require("cors");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const busContoller = require("./controllers/busController");
 const stationController = require("./controllers/stationController");
 const tripController = require("./controllers/tripController");
@@ -8,29 +7,22 @@ const bookingController = require("./controllers/bookingController");
 const userController = require("./controllers/userController");
 
 // database connection
-mongoose.connect("mongodb://localhost:27017/exclusive");
+mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true, useUnifiedTopology: true});
 const express = require("express");
 const path = require("path");
 
 const app = express();
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 busContoller(app, mongoose);
 stationController(app, mongoose);
 tripController(app, mongoose);
 bookingController(app, mongoose);
 userController(app, mongoose);
-
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
- */
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
